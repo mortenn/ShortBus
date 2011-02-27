10942fd5828bab9974a41691afd290cc7e435521
Morten Nilsen <morten@runsafe.no>
2011-02-27 17:03:51 +0100

    Refactorization and more xml code comments

--- ShortBus.Contracts/IServiceBusServer.cs
+++ ShortBus.Contracts/IServiceBusServer.cs
@@ -6,15 +6,32 @@ namespace ShortBus.Contracts
     [ServiceContract(Namespace = "http://shortbus.runsafe.no/contracts/2011/02/")]
     public interface IServiceBusServer
     {
+        /// <summary>
+        /// Subscribe a client to this server
+        /// </summary>
+        /// <param name="clientId">The clients' unique identifier</param>
+        /// <param name="endpoint">The endpoint the client expects messages on</param>
         [OperationContract(IsOneWay=true)]
         void Subscribe(Guid clientId, string endpoint);
 
+        /// <summary>
+        /// Remove a client subscription from this server
+        /// </summary>
+        /// <param name="clientId">The clients' unique identifier</param>
         [OperationContract(IsOneWay = true)]
         void Unsubscribe(Guid clientId);
 
+        /// <summary>
+        /// Send a message to all clients
+        /// </summary>
+        /// <param name="data">The message to send</param>
         [OperationContract(IsOneWay = true)]
         void Publish(ServiceBusEvent data);
 
+        /// <summary>
+        /// Respond to a heartbeat ping
+        /// </summary>
+        /// <param name="clientId">The clients' unique identifier</param>
         [OperationContract(IsOneWay = true)]
         void Heartbeat(Guid clientId);
     }
--- ShortBus.Server/ServerPump.cs
+++ ShortBus.Server/ServerPump.cs
@@ -14,10 +14,48 @@ namespace ShortBus
     [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
     public class ServerPump : IServiceBusServer
     {
+        /// <summary>
+        /// Sets up the message pump with heartbeat after 10 minutes with a 5 second timeout, on the private queue shortbus
+        /// If you change the queue naming in app.config, you must provide a new value to MessageQueueName
+        /// </summary>
+        public ServerPump()
+        {
+            ClientFactory = new MSMQClientFactory();
+            HeartbeatTimeout = 5000;
+            CheckHeartbeatAfter = new TimeSpan(0, 10, 0);
+            MessageQueueName = ".\\private$\\shortbus";
+        }
+
+        /// <summary>
+        /// A storage object that holds subscriptions. No default value
+        /// </summary>
         public ISubscriberStore Storage { get; set; }
 
+        /// <summary>
+        /// A factory object that creates proxies for communicating with the clients. Defaults to a MSMQClientFactory
+        /// </summary>
         public IFactory<IServiceBusClient> ClientFactory { get; set; }
 
+        /// <summary>
+        /// Wait this many milliseconds before removing a non-responsive subscriber. Defaults to 5 seconds.
+        /// </summary>
+        public double HeartbeatTimeout { get; set; }
+
+        /// <summary>
+        /// Send heartbeat requests to client after they have gone unseen for this amount of time. Defaults to 10 minutes.
+        /// </summary>
+        public TimeSpan CheckHeartbeatAfter { get; set; }
+
+        /// <summary>
+        /// The local message queue to get messages from. Defaults to ".\private$\shortbus".
+        /// </summary>
+        public string MessageQueueName { get; set; }
+
+        /// <summary>
+        /// Interface method used by clients
+        /// </summary>
+        /// <param name="clientId">Calling clients' ID</param>
+        /// <param name="endpoint">Calling clients' listening endpoint</param>
         public void Subscribe(Guid clientId, string endpoint)
         {
             Debug.WriteLine("{0} subscribed at {1}", clientId, endpoint);
@@ -25,6 +63,10 @@ namespace ShortBus
                 Storage.AddSubscriber(clientId, endpoint);
         }
 
+        /// <summary>
+        /// Interface method used by clients
+        /// </summary>
+        /// <param name="clientId">Calling clients' ID</param>
         public void Unsubscribe(Guid clientId)
         {
             Debug.WriteLine("{0} unsubscribed");
@@ -32,6 +74,10 @@ namespace ShortBus
                 Storage.RemoveSubscriber(clientId);
         }
 
+        /// <summary>
+        /// Interface method used by clients
+        /// </summary>
+        /// <param name="data">A message to broadcast</param>
         public void Publish(ServiceBusEvent data)
         {
             Debug.WriteLine("{0} published an event {1} ({2})", data.SourceSubscriber, data.EventName, data.EventId);
@@ -39,6 +85,10 @@ namespace ShortBus
             Storage.SubscriberSeen(data.SourceSubscriber);
         }
 
+        /// <summary>
+        /// Interface method used by clients
+        /// </summary>
+        /// <param name="clientId">Calling clients' ID</param>
         public void Heartbeat(Guid clientId)
         {
             if (clientId == Guid.Empty)
@@ -51,22 +101,29 @@ namespace ShortBus
             Storage.SubscriberSeen(clientId);
         }
 
+        /// <summary>
+        /// Start the message pump at the endpoint configured in app.config
+        /// </summary>
         public void Start()
         {
-            if (!MessageQueue.Exists(".\\private$\\shortbus"))
+            if (!MessageQueue.Exists(MessageQueueName))
             {
-                MessageQueue queue = MessageQueue.Create(".\\private$\\shortbus", true);
+                MessageQueue queue = MessageQueue.Create(MessageQueueName, true);
                 queue.Authenticate = false;
                 queue.EncryptionRequired = EncryptionRequired.Optional;
             }
             if (host == null)
                 host = new ServiceHost(this);
             host.Open();
-            pulse = new Timer(5000);
+
+            pulse = new Timer(HeartbeatTimeout);
             pulse.Elapsed += (s, e) => SendHeartBeat();
             pulse.Start();
         }
 
+        /// <summary>
+        /// Stop the message pump
+        /// </summary>
         public void Stop()
         {
             host.Close();
@@ -74,6 +131,11 @@ namespace ShortBus
             pulse = null;
         }
 
+        /// <summary>
+        /// Send a message to a subscription
+        /// </summary>
+        /// <param name="subscription">The subscription the message is destined for</param>
+        /// <param name="data">The message to send to the subscriber</param>
         void SendMessage(Subscriber subscription, ServiceBusEvent data)
         {
             if (!clients.ContainsKey(subscription.SubscriberId))
@@ -89,40 +151,60 @@ namespace ShortBus
             }
         }
 
+        /// <summary>
+        /// Send heartbeat messages to clients we haven't seen in a while
+        /// </summary>
         void SendHeartBeat()
         {
             lock (lockHandle)
             {
-                foreach (Subscriber sub in Storage.GetUnseenSubscribersSince(DateTime.Now - new TimeSpan(0, 10, 0)))
+                foreach (Subscriber sub in Storage.GetUnseenSubscribersSince(DateTime.Now - CheckHeartbeatAfter))
                 {
                     if (HeartbeatMonitorList.Contains(sub.SubscriberId))
-                    {
-                        Debug.WriteLine("Not feeling a pulse for {0}", sub.SubscriberId);
+                        SubscriberTimedOut(sub);
 
-                        Storage.RemoveSubscriber(sub.SubscriberId);
-                        HeartbeatMonitorList.Remove(sub.SubscriberId);
-                        continue;
-                    }
-                    if (!clients.ContainsKey(sub.SubscriberId))
-                        clients.Add(sub.SubscriberId, ClientFactory.Connect(sub.Endpoint));
-                    try
-                    {
-                        Debug.WriteLine("Checking {0} for a pulse", sub.SubscriberId);
-                        clients[sub.SubscriberId].Heartbeat();
-                        HeartbeatMonitorList.Add(sub.SubscriberId);
-                    }
-                    catch (EndpointNotFoundException)
-                    {
-                        Unsubscribe(sub.SubscriberId);
-                    }
+                    else
+                        SendHeartBeat(sub);
                 }
             }
         }
 
+        /// <summary>
+        /// Sends a heartbeat message to a given subscription
+        /// </summary>
+        /// <param name="sub">The subscription to ping</param>
+        void SendHeartBeat(Subscriber sub)
+        {
+            if (!clients.ContainsKey(sub.SubscriberId))
+                clients.Add(sub.SubscriberId, ClientFactory.Connect(sub.Endpoint));
+            try
+            {
+                Debug.WriteLine("Checking {0} for a pulse", sub.SubscriberId);
+                clients[sub.SubscriberId].Heartbeat();
+                HeartbeatMonitorList.Add(sub.SubscriberId);
+            }
+            catch (EndpointNotFoundException)
+            {
+                Unsubscribe(sub.SubscriberId);
+            }
+        }
+
+        /// <summary>
+        /// Removes a subscriber that didn't respond to a heartbeat request
+        /// </summary>
+        /// <param name="sub"></param>
+        void SubscriberTimedOut(Subscriber sub)
+        {
+            Debug.WriteLine("Not feeling a pulse for {0}", sub.SubscriberId);
+
+            Storage.RemoveSubscriber(sub.SubscriberId);
+            HeartbeatMonitorList.Remove(sub.SubscriberId);
+        }
+
+        ServiceHost host;
         Timer pulse;
         Dictionary<Guid, IServiceBusClient> clients = new Dictionary<Guid, IServiceBusClient>();
         List<Guid> HeartbeatMonitorList = new List<Guid>();
         static object lockHandle = new object();
-        ServiceHost host;
     }
 }
